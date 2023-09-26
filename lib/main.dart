6e539504fd68340bb8e70aa93d496bffc449dda1
LeonasAnony <29leon42005@gmail.com>
2023-09-26 08:22:08 +0200

    Flutter TCP Example

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:car_app/app.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(App());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
-    );
-  }
-}
