6aac7c7eb296fd556d2023534589b091cfb7e7d1
LeonasAnony <29leon42005@gmail.com>
2023-10-24 13:25:32 +0200

    matate

--- main.py
+++ main.py
@@ -4,105 +4,12 @@ gi.require_version("Gtk", "3.0")
 from gi.repository import Gtk, GLib
 
 
-class EntryWindow(Gtk.Window):
+class MainWindow(Gtk.Window):
     def __init__(self):
-        super().__init__(title="Entry Demo")
-        self.set_size_request(200, 100)
-
-        self.timeout_id = None
-
-        vbox = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=6)
-        self.add(vbox)
-
-
-
-
-
-        self.entry = Gtk.Entry()
-        self.entry.set_text("Hello World")
-        vbox.pack_start(self.entry, True, True, 0)
-
-        hbox = Gtk.Box(spacing=6)
-        vbox.pack_start(hbox, True, True, 0)
-
-        self.check_editable = Gtk.CheckButton(label="Editable")
-        self.check_editable.connect("toggled", self.on_editable_toggled)
-        self.check_editable.set_active(True)
-        hbox.pack_start(self.check_editable, True, True, 0)
-
-        self.check_visible = Gtk.CheckButton(label="Visible")
-        self.check_visible.connect("toggled", self.on_visible_toggled)
-        self.check_visible.set_active(True)
-        hbox.pack_start(self.check_visible, True, True, 0)
-
-        self.pulse = Gtk.CheckButton(label="Pulse")
-        self.pulse.connect("toggled", self.on_pulse_toggled)
-        self.pulse.set_active(False)
-        hbox.pack_start(self.pulse, True, True, 0)
-
-        self.icon = Gtk.CheckButton(label="Icon")
-        self.icon.connect("toggled", self.on_icon_toggled)
-        self.icon.set_active(False)
-        hbox.pack_start(self.icon, True, True, 0)
-
-    def on_editable_toggled(self, button):
-        value = button.get_active()
-        self.entry.set_editable(value)
-
-    def on_visible_toggled(self, button):
-        value = button.get_active()
-        self.entry.set_visibility(value)
-
-    def on_pulse_toggled(self, button):
-        if button.get_active():
-            self.entry.set_progress_pulse_step(0.2)
-            # Call self.do_pulse every 100 ms
-            self.timeout_id = GLib.timeout_add(100, self.do_pulse, None)
-        else:
-            # Don't call self.do_pulse anymore
-            GLib.source_remove(self.timeout_id)
-            self.timeout_id = None
-            self.entry.set_progress_pulse_step(0)
-
-    def do_pulse(self, user_data):
-        self.entry.progress_pulse()
-        return True
-
-    def on_icon_toggled(self, button):
-        if button.get_active():
-            icon_name = "system-search-symbolic"
-        else:
-            icon_name = None
-        self.entry.set_icon_from_icon_name(Gtk.EntryIconPosition.PRIMARY, icon_name)
-
-class DialogConnect(Gtk.Dialog):
-    def __init__(self, parent):
-        super().__init__(title="Connect", transient_for=parent, flags=0)
-        self.add_buttons(
-            Gtk.STOCK_CANCEL, Gtk.ResponseType.CANCEL, Gtk.STOCK_OK, Gtk.ResponseType.OK
-        )
-
-        self.set_default_size(150, 100)
-
-        label = Gtk.Label(label="Connect with ESP32 over TCP")
-
-        box = self.get_content_area()
-        box.add(label)
-        self.show_all()
-
-
-class DialogWindow(Gtk.Window):
-    def __init__(self):
-        Gtk.Window.__init__(self, title="Dialog Example")
+        Gtk.Window.__init__(self, title="ESP32 Interface")
 
         self.set_border_width(6)
 
-        button = Gtk.Button(label="Open dialog")
-        button.connect("clicked", self.on_button_clicked)
-
-        self.add(button)
-
-    def on_button_clicked(self, widget):
         dialog = DialogConnect(self)
         response = dialog.run()
 
@@ -113,13 +20,22 @@ class DialogWindow(Gtk.Window):
 
         dialog.destroy()
 
+class DialogConnect(Gtk.Dialog):
+    def __init__(self, parent):
+        super().__init__(title="Connect", transient_for=parent, flags=0)
+        self.add_buttons(
+            Gtk.STOCK_CANCEL, Gtk.ResponseType.CANCEL, Gtk.STOCK_OK, Gtk.ResponseType.OK
+        )
 
-win = DialogWindow()
+        self.set_default_size(200, 200)
+
+        label = Gtk.Label(label="Connect with ESP32 over TCP")
+
+        box = self.get_content_area()
+        box.add(label)
+        self.show_all()
+
+win = MainWindow()
 win.connect("destroy", Gtk.main_quit)
 win.show_all()
 Gtk.main()
-
-#win = EntryWindow()
-#win.connect("destroy", Gtk.main_quit)
-#win.show_all()
-#Gtk.main()
