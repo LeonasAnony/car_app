97d683118d861cf521662657b350c254cee2237d
LeonasAnony <29leon42005@gmail.com>
2023-10-24 13:57:21 +0200

    KMS

--- main.py
+++ main.py
@@ -20,31 +20,30 @@ class MainWindow(Gtk.Window):
 
 		dialog.destroy()
 
-class DialogConnect(Gtk.Dialog):
-	def __init__(self, parent):
-		super().__init__(title="Connect", transient_for=parent, flags=0)
-		self.add_buttons(
-			Gtk.STOCK_CANCEL, Gtk.ResponseType.CANCEL, Gtk.STOCK_OK, Gtk.ResponseType.OK
-		)
+class ConnectWindow(Gtk.Dialog):
+	def __init__(self):
+		super().__init__(title="ESP32 Connect")
+		#self.add_buttons(
+		#	Gtk.STOCK_CANCEL, Gtk.ResponseType.CANCEL, Gtk.STOCK_OK, Gtk.ResponseType.OK
+		#)
 
 		self.set_default_size(600, 400)
 
 		vbox = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=6)
 		self.add(vbox)
 		
-		label = Gtk.Label(label="Connect with ESP32 over TCP")
-		vbox = self.get_content_area()
-		vbox.add(label)
+		self.label = Gtk.Label(label="Connect with ESP32 over TCP")
+		vbox.pack_start(self.label, True, True, 0)
 
-		self.entry = Gtk.Entry()
-		self.entry.set_text("IP Address")
-		vbox.pack_start(self.entry, True, True, 0)
+		self.entryip = Gtk.Entry()
+		self.entryip.set_text("IP Address")
+		vbox.pack_start(self.entryip, True, True, 0)
 		
-		self.entry = Gtk.Entry()
-		self.entry.set_text("Port")
-		vbox.pack_start(self.entry, True, True, 0)
+		self.entryport = Gtk.Entry()
+		self.entryport.set_text("Port")
+		vbox.pack_start(self.entryport, True, True, 0)
 
-win = MainWindow()
+win = ConnectWindow()
 win.connect("destroy", Gtk.main_quit)
 win.show_all()
 Gtk.main()
