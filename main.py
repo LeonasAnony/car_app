684e1da71f4e81433d41f0b52ce1b950e8224f25
LeonasAnony <29leon42005@gmail.com>
2023-11-17 12:08:21 +0100

    ledHeartbeat Dimm

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from mainWindow import MainWindow
 gi.require_version("Gtk", "3.0")
 from gi.repository import Gtk
 
-def open_main_window(tn):
+def open_main_window(widget, tn):
 	mainWin = MainWindow(tn)
 	mainWin.connect("destroy", Gtk.main_quit)
 	mainWin.connect("delete-event", Gtk.main_quit)
@@ -17,6 +17,10 @@ def open_main_window(tn):
 	listenThread.start()
 
 def telnet_listen(tn, win): #TODO: eventually move this to ./helper/
+	ledHeartbeatGreen = win.ledHeartbeat.rgb_color[1]
+	if(ledHeartbeatGreen > 64):
+		win.ledHeartbeat.set_color(0, (ledHeartbeatGreen-2), 0)
+
 	match("telnet_read"): #TODO: read Telnet and switch
 		case("Heartbeat"):
 			win.ledHeartbeat.set_color(0,255,0)
@@ -24,10 +28,6 @@ def telnet_listen(tn, win): #TODO: eventually move this to ./helper/
 			win.ledSwitch.set_color(0,255,0)
 		case("Switch OFF"):
 			win.ledSwitch.set_color(255,0,0)
-
-	if(win.ledHeartbeat):
-		pass
-		#TODO: slowly dimm Heartbeat LED
 
 if __name__ == "__main__":
 	tn = telnetlib.Telnet()
