a94bd80addba2831376cb72662291f7a38333175
LeonasAnony <29leon42005@gmail.com>
2023-11-17 14:07:48 +0100

    make app a class, read telnet and set color

--- main.py
+++ main.py
@@ -5,35 +5,41 @@ from connectWindow import ConnectWindow
 from mainWindow import MainWindow
 
 gi.require_version("Gtk", "3.0")
-from gi.repository import Gtk
+from gi.repository import Gtk, GObject
 
-def open_main_window(widget, tn):
-	mainWin = MainWindow(tn)
-	mainWin.connect("destroy", Gtk.main_quit)
-	mainWin.connect("delete-event", Gtk.main_quit)
-	mainWin.show_all()
+class ESP32_Interface():
+	def __init__(self):
+		self.tn = telnetlib.Telnet()
+		conWin = ConnectWindow(self.tn)
+		conWin.connect("delete-event", Gtk.main_quit)
+		conWin.connect("destroy", self.open_main_window)
+		conWin.show_all()
+		self.mainThread = threading.Thread(target=Gtk.main)
+		self.mainThread.start()
 
-	listenThread = threading.Thread(target=telnet_listen, args=(tn, mainWin))
-	listenThread.start()
+	def stop_app(self, *args):
+		Gtk.main_quit()
 
-def telnet_listen(tn, win): #TODO: eventually move this to ./helper/
-	ledHeartbeatGreen = win.ledHeartbeat.rgb_color[1]
-	if(ledHeartbeatGreen > 64):
-		win.ledHeartbeat.set_color(0, (ledHeartbeatGreen-2), 0)
+	def open_main_window(self, *args):
+		self.mainWin = MainWindow(self.tn)
+		self.mainWin.connect("destroy", self.stop_app)
+		self.mainWin.connect("delete-event", self.stop_app) #TODO: add quit fun which stops threads
+		self.mainWin.show_all()
+		GObject.timeout_add(100, self.mainWin.dimm_led, self.mainWin.ledHeartbeat, "g")
 
-	match("telnet_read"): #TODO: read Telnet and switch
-		case("Heartbeat"):
-			win.ledHeartbeat.set_color(0,255,0)
-		case("Switch ON"):
-			win.ledSwitch.set_color(0,255,0)
-		case("Switch OFF"):
-			win.ledSwitch.set_color(255,0,0)
+		self.listenThread = threading.Thread(target=self.telnet_listen)
+		self.listenThread.start()
+
+	def telnet_listen(self): #TODO: eventually move this to ./helper/#
+		while(True):
+			msg = self.tn.read_until(b"\r\n")
+			print(msg)
+			if(b"<TICK>" in msg):
+				self.mainWin.ledHeartbeat.set_color(0,255,0)
+			elif(msg == b"<EVENT><KEY>switch</KEY><VALUE>on</VALUE></EVENT>\r\n"):
+				self.mainWin.ledSwitch.set_color(0,255,0)
+			elif(msg == b"<EVENT><KEY>switch</KEY><VALUE>off</VALUE></EVENT>\r\n"):
+				self.mainWin.ledSwitch.set_color(255,0,0)
 
 if __name__ == "__main__":
-	tn = telnetlib.Telnet()
-	conWin = ConnectWindow(tn)
-	conWin.connect("delete-event", Gtk.main_quit)
-	conWin.connect("destroy", open_main_window, tn)
-	conWin.show_all()
-	mainThread = threading.Thread(target=Gtk.main)
-	mainThread.start()
+	ESP32_Interface()
